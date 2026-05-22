d14291d948b1556d028c0946bdf3a97b27ea04f7
mir Y <2922027393@qq.com>
2026-05-22 16:47:17 +0800

    Split validation constants

---
+++ src/lib/validation/constants.ts
@@ -0,0 +1,5 @@
+import type { FieldKey } from "../types";
+
+export const identifierFields: FieldKey[] = ["gclid", "gbraid", "wbraid", "email", "phone"];
+export const clickIdFields: FieldKey[] = ["gclid", "gbraid", "wbraid"];
+export const addressFields: FieldKey[] = ["first_name", "last_name", "street_address", "city", "state", "country", "zip"];
